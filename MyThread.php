4c43fe4213285dd18b62d0e87666ec35afe254d1
Art Murr <12go12go@mail.ru>
2020-08-22 01:00:22 +0800

    added assets, tasks.json, Loop for tasks

--- MyThread.php
+++ MyThread.php
@@ -1,11 +1,12 @@
 <?php 
 class myTread extends Thread {
 
-	public function __construct ($method){
+	public function __construct ($method, $params){
 		$this->method = $method;
+		$this->params = $params;
 	}
 	public function run() {
-		echo "Method: ".$this->method;
+		//echo "Method: ".$this->method;
 		switch ($this->method) {
 			case 'processPayment':
 				$this->processPayment();
@@ -15,22 +16,26 @@ class myTread extends Thread {
 			break;
 			
 			default:
-				echo "unknown Default %)\n";
-				break;
+				echo "unknown Operation %)\n";
+			break;
 		}
 	}
 
 	public function processPayment(){
-		echo "processing Payment: ".date("h:i:s")."\n";
+		( isset($this->params['account_id']) ) ? $account_id = $this->params['account_id'] : $account_id = "N/A";
+		( isset($this->params['amount']) ) ? $amount = $this->params['amount'] : $amount = "N/A";
+		echo "[Processing Payment] with Params: account_id = ".$account_id." and amount = ".$amount." at ".date("h:i:s")."\n\n";
 		sleep(1);
 	}
 
 	public function sendLead(){
-		echo "sending Lead: ".date("h:i:s")."\n";
+		( isset($this->params['lead_id']) ) ? $lead_id = $this->params['lead_id'] : $lead_id = "N/A";
+		echo "[Sending Lead] with Params: lead_id = ".$lead_id." at ".date("h:i:s")."\n\n";
 		sleep(3);
 	}
 
 	private $method = null;
+	private $params = null;
 };
 
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,50 @@
 <?php
 require_once 'MyThread.php';
 
-$threads = 6;
+$data = file_get_contents("tasks.json");
+$json_data = json_decode($data, TRUE);
+$tasks_counter = sizeOf($json_data);
+
+//$threads is configurable up to the limit of the CPU cores
+$threads = 4;
 
 $start = microtime(true);
 echo "Start: ".date("d-m-yy h:i:s")."\n";
+echo ".....................................................................\n";
+/*
+	initialize threads to process tasks while they exist in $json_data
+*/
+while($tasks_counter > 0) {
 
-for( $i = 0; $i < $threads; $i++){
-    ($i == 0 || $i == $threads - 1) ? $pool[] = new myTread('processPayment') : $pool[] = new myTread('sendLead');
+	($threads > $tasks_counter) ? $limit = $tasks_counter : $limit = $threads;
+
+	for( $i = 0; $i < $limit; $i++){
+	    if ($json_data[$i]['category'] == 'account' && $json_data[$i]['task'] == 'processPayment' && isset($json_data[$i]['data'])) {
+	    	$pool[] = new myTread('processPayment',$json_data[$i]['data']);
+	    }else if ($json_data[$i]['category'] == 'amocrm' && $json_data[$i]['task'] == 'sendLead'  && isset($json_data[$i]['data'])) {
+	    	$pool[] = new myTread('sendLead', $json_data[$i]['data']);
+	    }else {
+	    	echo "At position $i data set is incorrect \n".$json_data[$i]['category'].$json_data[$i]['task'];
+	    }
+	}
+
+	foreach($pool as $worker){
+	    $worker->start();
+	}
+	foreach($pool as $worker){
+	    $worker->join();
+	}
+	
+	$pool = [];
+	//delete tasks which already sent for processing in new Thread
+	array_splice($json_data, 0, $threads);
+	//recalculate counter
+	$tasks_counter = sizeOf($json_data);
+	echo ".....................................................................\n";
 }
 
-foreach($pool as $worker){
-    $worker->start();
-}
-foreach($pool as $worker){
-    $worker->join();
-}
-printf("Done for %.2f seconds" . "\n", microtime(true) - $start);
+$seconds = microtime(true) - $start;
+echo "Done in ".number_format($seconds,2)." seconds\n";
 echo "Ended: ".date("d-m-yy h:i:s")."\n";
+
 ?>
